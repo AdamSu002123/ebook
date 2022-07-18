d89ce22308b348c243f13df4aa0cead3db97507f
Adam Su <gih96984@gmail.com>
2022-07-18 11:33:29 +0800

    Initial Commit

---
+++ ebook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ebook
+//
+//  Created by gih96984 on 2022/7/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
